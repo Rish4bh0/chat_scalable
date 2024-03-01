c4b93f5d85ab1e440842eef25a34621087a16f76
Rish4bh0 <122444548+Rish4bh0@users.noreply.github.com>
2024-03-01 15:56:31 +0545

    added redis pubsub used aiven

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,19 @@
 import {Server} from 'socket.io'
+import Redis from 'ioredis'
+
+
+const pub = new Redis({
+    host: 'redis-e7c6571-rishabh9803451416-a557.a.aivencloud.com',
+    port: 24112,
+    username: 'default',
+    password: 'AVNS_y6taLR7lbgks__VWBol'
+});
+const sub = new Redis({
+    host: 'redis-e7c6571-rishabh9803451416-a557.a.aivencloud.com',
+    port: 24112,
+    username: 'default',
+    password: 'AVNS_y6taLR7lbgks__VWBol'
+});
 
 class SocketService {
     private _io: Server;
@@ -10,6 +25,7 @@ class SocketService {
                 origin:"*"
             }
         })
+        sub.subscribe('MESSAGES')
     }
 
     public initListeners() {
@@ -20,9 +36,16 @@ class SocketService {
             
             socket.on(`event:message`, async ({message}: {message: string}) => {
                 console.log("New message received", message);
-               
+               // publish mesage to redis
+               await pub.publish('MESSAGES', JSON.stringify({message}))
              })
         });
+
+        sub.on('message', (channel, message) => {
+            if (channel === 'MESSAGES') {
+                io.emit("message", message)
+            }
+        })
     }
 
 
